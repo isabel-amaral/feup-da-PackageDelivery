727e31e7e8fc0a25245b4632d3ad8acb61c74806
Guilherme Moreira <gui29102002@gmail.com>
2022-04-17 15:13:01 +0000

    Added coefficient (extra) function

--- src/Company.cpp
+++ src/Company.cpp
@@ -53,57 +53,67 @@ void Company::updateProfit(const int &profit) {
 }
 
 void Company::deliverNormalDelivery(const NormalDelivery &normalDelivery) {
-    //TODO
+    normalDeliveries.remove(normalDelivery);
 }
 
 void Company::deliverExpressDelivery(const ExpressDelivery &expressDelivery) {
     //TODO
 }
 
+/*____________cenário 1____________*/
 void Company::scenery1() {
     list<pair<int,int>> v1;
 
+    // Different ways of sorting the lists to get best solution
     drivers.sort(Driver::sorting_driver_volume);
 
-    normalDeliveries.sort(NormalDelivery::sorting_package_volume);
+    normalDeliveries.sort(NormalDelivery::compareVolume);
     v1.push_back(Company::DriverCount());
-    normalDeliveries.sort(NormalDelivery::sorting_package_weight);
+    normalDeliveries.sort(NormalDelivery::compareWeight);
     v1.push_back(Company::DriverCount());
-    normalDeliveries.sort(NormalDelivery::sorting_package_addition);
+    normalDeliveries.sort(NormalDelivery::compareAddition);
     v1.push_back(Company::DriverCount());
 
     drivers.sort(Driver::sorting_driver_weight);
 
-    normalDeliveries.sort(NormalDelivery::sorting_package_volume);
+    normalDeliveries.sort(NormalDelivery::compareVolume);
     v1.push_back(Company::DriverCount());
-    normalDeliveries.sort(NormalDelivery::sorting_package_weight);
+    normalDeliveries.sort(NormalDelivery::compareWeight);
     v1.push_back(Company::DriverCount());
-    normalDeliveries.sort(NormalDelivery::sorting_package_addition);
+    normalDeliveries.sort(NormalDelivery::compareAddition);
     v1.push_back(Company::DriverCount());
 
     drivers.sort(Driver::sorting_driver_addition);
 
-    normalDeliveries.sort(NormalDelivery::sorting_package_volume);
+    normalDeliveries.sort(NormalDelivery::compareVolume);
     v1.push_back(Company::DriverCount());
-    normalDeliveries.sort(NormalDelivery::sorting_package_weight);
+    normalDeliveries.sort(NormalDelivery::compareWeight);
     v1.push_back(Company::DriverCount());
-    normalDeliveries.sort(NormalDelivery::sorting_package_addition);
+    normalDeliveries.sort(NormalDelivery::compareAddition);
     v1.push_back(Company::DriverCount());
 
     v1.sort(Company::sorting_by_missing);
-    
-    if (v1.front().second > 0)
-        cout << "There are " << v1.front().first << " drivers needed to carry all the packages, although " << v1.front().second << " packages didn't fit." << endl;
-    else {
-        cout << "There are " << v1.front().first << " drivers needed to carry all the packages." << endl;
+
+    printResults1(v1.front().first,v1.front().second);
+}
+
+void Company::printResults1(int driver_count, int missing_packages) {
+    int coefficient = (normalDeliveries.size()/(normalDeliveries.size()+missing_packages))*100;
+    if (missing_packages > 0) {
+        cout << "There are " << driver_count << " drivers needed to deliver all the packages, although " << missing_packages << " packages didn't fit." << endl;
     }
+    else {
+        cout << "There are " << driver_count << " drivers needed to deliver all the packages." << endl;
+    }
+    cout << "The coefficient between the number of packages requested and delivered is " << coefficient << "%." << endl;
 }
 
 pair<int,int> Company::DriverCount() {
-    int missing = 0;
-    int ans = 1;
+    setProfit(0);
+    int missing = 0; // number of packages which didn't fit with available drivers
+    int ans = 1; // number of drivers needed to transport the packages
 
-    vector<Driver> remaining;
+    vector<Driver> remaining; // weight and volume remaining in drivers selected for deliveries
     remaining.push_back(drivers.front());
 
     for (NormalDelivery delivery : normalDeliveries) {
@@ -112,21 +122,27 @@ pair<int,int> Company::DriverCount() {
             if ((delivery.getWeight() <= remaining[j].getMaxWeight()) && (delivery.getVolume() <= remaining[j].getMaxVolume())) {
                 remaining[j].setMaxWeight(remaining[j].getMaxWeight()-delivery.getWeight());
                 remaining[j].setMaxVolume(remaining[j].getMaxVolume()-delivery.getVolume());
+                setProfit(getProfit()+delivery.getDeliveryFee());
                 break;
             }
         }
 
-        if (j == ans) {
+        if (j == ans) { // if package doesn't fit in any of the drivers selected for deliveries
             if(get(drivers,ans).getMaxWeight()-delivery.getWeight() >= 0 && get(drivers,ans).getMaxVolume()-delivery.getVolume() >= 0) {
                 get(drivers,ans).setMaxWeight(get(drivers,ans).getMaxWeight()-delivery.getWeight());
                 get(drivers,ans).setMaxVolume(get(drivers,ans).getMaxVolume()-delivery.getVolume());
                 remaining.push_back(get(drivers,ans));
+                setProfit(getProfit()+delivery.getDeliveryFee());
                 ans++;
             }
-            else {
+            else { // if package doesn't fit in any of available drivers, package cannot be transported
                 missing++;
             }
         }
+    }
+
+    for (Driver driver : remaining) {
+        setProfit(getProfit()-driver.getDeliveryCost());
     }
 
     pair<int,int> v1;
@@ -136,10 +152,21 @@ pair<int,int> Company::DriverCount() {
 }
 
 bool Company::sorting_by_missing(const pair<int,int> &v1, const pair<int,int> &v2) {
+    // sort by number of missing packages, if  the same, by number of drivers necessary, in ascending order
     if (v1.second==v2.second)
         return v1.first<v2.first;
     return v1.second<v2.second;
 }
+
+Driver Company::get(list<Driver> _list, int _i){
+    // get value from a list "_list" in the "_i" index
+    list<Driver>::iterator it = _list.begin();
+    for(int i=0; i<_i; i++){
+        ++it;
+    }
+    return *it;
+}
+
 /*____________cenário 3____________*/
 void Company::printResults() {
     int totalTimeSpent = 0;
@@ -176,10 +203,53 @@ void Company::scenery3() {
     printResults();
 }
 
-Driver Company::get(list<Driver> _list, int _i){
-    list<Driver>::iterator it = _list.begin();
-    for(int i=0; i<_i; i++){
-        ++it;
-    }
-    return *it;
+/*____________cenário 2____________*/
+void Company::printResults2(int &numDeliveries) const {
+    cout << "No maximo sera possivel entregar " << numDeliveries << " encomendas." << endl;
+    cout << "A empresa fica com um lucro de " << profit << " euros." << endl;
+}
+void Company::scenery2() {
+    int bestProfit, auxProfit, totalProfit = 0, numDeliveries = 0;
+    bool foundDriver;
+    list<Driver>::iterator bestDriver;
+    list<Driver> copyDrivers = drivers;
+    list<NormalDelivery> copyDeliveries = normalDeliveries;
+
+    normalDeliveries.sort(NormalDelivery::compareFee);
+    drivers.sort(Driver::compareCost);
+
+    do {
+        foundDriver = false;
+        bestProfit = 0;
+        for(auto driver = drivers.begin(); driver != drivers.end(); driver++){
+            auxProfit = totalProfit;
+            for (auto &delivery : normalDeliveries){
+                if (auxProfit + delivery.getDeliveryFee() - driver->getDeliveryCost() >= 0 && driver->addOrder(delivery)){
+                    auxProfit += delivery.getDeliveryFee() - driver->getDeliveryCost();
+                }
+            }
+            if (auxProfit > bestProfit){ //TODO: INCLUIR A CONDICAO DE MAIOR NUMERO DE ENCOMENDAS CASO auxProfit == bestProfit?
+                bestProfit = auxProfit;
+                bestDriver = driver;
+                foundDriver = true;
+            }
+        }
+        if (!foundDriver) continue;
+        totalProfit = bestProfit;
+        numDeliveries += bestDriver->getOrdersToDeliver().size();
+        for (auto &delivery : bestDriver->getOrdersToDeliver()) deliverNormalDelivery(delivery);
+        drivers.erase(bestDriver);
+        for (auto &driver : drivers) driver.removeOrders();
+
+    }while(foundDriver);
+    /*
+    if (!normalDeliveries.empty()){
+        drivers =copyDrivers;
+        normalDeliveries = copyDeliveries;
+        int numDeliveriesScenery1 = scenery1().;
+        if (numDeliveriesScenery1 > numDeliveries) numDeliveries = numDeliveriesScenery1;
+        else profit = totalProfit;
+    }*/
+    profit = totalProfit;
+    printResults2(numDeliveries);
 }
--- src/NormalDelivery.cpp
+++ src/NormalDelivery.cpp
@@ -30,18 +30,23 @@ void NormalDelivery::setDeliveryFee(int fee) {
     deliveryFee = fee;
 }
 
-bool NormalDelivery::sorting_package_weight(NormalDelivery n1, NormalDelivery n2) {
+bool NormalDelivery::compareWeight(const NormalDelivery& n1, const NormalDelivery& n2) {
     return n1.getWeight() > n2.getWeight();
 }
 
-bool NormalDelivery::sorting_package_volume(NormalDelivery n1, NormalDelivery n2) {
+bool NormalDelivery::compareVolume(const NormalDelivery& n1, const NormalDelivery& n2) {
     return n1.getVolume() > n2.getVolume();
 }
 
-bool NormalDelivery::sorting_package_addition(NormalDelivery n1, NormalDelivery n2) {
+bool NormalDelivery::compareAddition(const NormalDelivery& n1, const NormalDelivery& n2) {
     return n1.getVolume()+n1.getWeight() > n2.getVolume()+n2.getWeight();
 }
 
 bool NormalDelivery::compareFee(const NormalDelivery &a, const NormalDelivery &b) {
+    //if (a.deliveryFee == b.deliveryFee) return a.weight + a.volume < b.weight + b.volume; TODO: Decidir se deve-se considerar este caso ou não
     return a.deliveryFee > b.deliveryFee;
+}
+
+bool NormalDelivery::operator==(const NormalDelivery &delivery) const {
+    return volume == delivery.volume && weight == delivery.weight && deliveryFee == delivery.deliveryFee;
 }
