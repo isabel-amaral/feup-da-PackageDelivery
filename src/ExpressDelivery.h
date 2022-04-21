673127576f8b976d86f44d8face8458d5f94d021
Milena Gouveia <milenaagouveia@gmail.com>
2022-04-22 00:52:27 +0100

    ExpressDelivery documentation

--- src/ExpressDelivery.h
+++ src/ExpressDelivery.h
@@ -2,13 +2,37 @@
 #define FEUP_DA_PACKAGEDELIVERY_EXPRESSDELIVERY_H
 
 class ExpressDelivery {
+    /**
+     * Estimated time for the order delivery.
+     */
     int estimatedDeliveryTime;
+    /**
+     * Order delivery priority.
+     */
     int priority;
 public:
     ExpressDelivery(int edt);
+    /**
+     *
+     * @return Estimated time for the order delivery.
+     */
     int getEstimatedDeliveryTime() const;
+    /**
+     *
+     * @return Order delivery priority.
+     */
     int getPriority() const;
+    /**
+     * Increments the order delivery priority.
+     */
     void incrementPriority();
+    /**
+     * Compares orders in descending order according to their "priority" attribute, if they are the same,
+     * compares them in ascending order according to their "estimatedDeliveryTime" attribute.
+     * @param n1 Express Delivery 1
+     * @param n2 Express Delivery 1
+     * @return true if @param n1 is smaller than @param n2, else returns false.
+     */
     static bool compareExpressDeliveries(const ExpressDelivery& n1, const ExpressDelivery& n2);
 };
 
